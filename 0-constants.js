43c9f2ed8781e1c4dd2e11672aeb1c823c36215d
Abel-alx-github <abeltibeso323@gmail.com>
2024-05-27 16:43:13 +0000

    use the right variable declaration const vs let

---
+++ 0-constants.js
@@ -0,0 +1,19 @@
+/*
+ a module contain 3 function defintaions
+ */
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
