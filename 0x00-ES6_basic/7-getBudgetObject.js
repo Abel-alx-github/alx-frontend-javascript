e5cfe5bf508237973f3c331c32f00963dbd92c06
Abel-alx-github <abeltibeso323@gmail.com>
2024-05-27 20:00:32 +0000

    Modify the function’s budget object to simply use the keyname instead

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+/*
+a module contain function defination
+*/
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income, gdp, capita,
+  };
+
+  return budget;
+}
