74af51e84e3b732eea1ce77cc84b4b6bc2dbb2d6
Abel-alx-github <abeltibeso323@gmail.com>
2024-05-29 20:21:44 +0000

    return a promise. The parameter is a boolean.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+/*
+* a module contains function defination
+*/
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(new Error('The fake API is not working currently'));
+  });
+}
