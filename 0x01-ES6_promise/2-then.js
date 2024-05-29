e56baff2471f106f1651d55856d5f0f60561d5e5
Abel-alx-github <abeltibeso323@gmail.com>
2024-05-29 20:58:03 +0000

    Append three handlers to the function

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+/*
+ * a module define function
+ */
+
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => {
+        console.log('Got a response from the API');
+        resolve({ status: 200, body: 'success' });
+      }).catch(() => reject(new Error()));
+  });
+}
