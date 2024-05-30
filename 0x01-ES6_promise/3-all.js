71ad693360fa40e9b187783a42e59094f37350f4
Abel-alx-github <abeltibeso323@gmail.com>
2024-05-30 07:16:50 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,6 +7,6 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((respond) => console.log(respond[0].body, respond[1].firstName, respond[1].lastName))
+    .then((respond) => console.log(`${respond[0].body} ${respond[1].firstName} ${respond[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
