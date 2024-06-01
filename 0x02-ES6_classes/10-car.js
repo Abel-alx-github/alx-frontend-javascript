dcd7d0ea55b6ce4889bb4f74da80dd3445831f37
Abel-alx-github <abeltibeso323@gmail.com>
2024-06-01 13:07:16 +0000

    Implement a class named Car: with method cloneCar

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,20 @@
+/*
+ * a module contain class defination
+ */
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
