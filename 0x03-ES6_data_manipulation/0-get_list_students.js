85f391bf803ca59eac0757ff9c5d58b91e88879c
Abel-alx-github <abeltibeso323@gmail.com>
2024-06-03 12:51:44 +0000

    Create a function named getListStudents that returns an array of objects.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/*
+ * a module conatin function defination
+*/
+
+export default function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
