907e2bba67901a74b3fd1d0b2369e939f81b0019
Abel-alx-github <abeltibeso323@gmail.com>
2024-06-04 20:10:09 +0000

    Create a function: returns a string of all set values, start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -10,7 +10,7 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || !(set instanceof Set)) { return ''; }
 
-  const array = Array.from(set).filter((string) => string.startsWith(startString));
+  const array = Array.from(set).filter((string) => typeof string === 'string' && string.startsWith(startString));
 
   const newString = array.map((string) => string.slice(startString.length));
   return newString.join('-');
